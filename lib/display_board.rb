07125903da0e7ee39b6f5f7cb2878ada7af5201a
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-05 04:13:17 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,12 +2,9 @@
 # out the current state.
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def display_board(board)
-  puts "   |   |   "
+  puts #{board[0]}
   puts "-----------"
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
 end
-display_board(board)
-board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
-display_board(board)
