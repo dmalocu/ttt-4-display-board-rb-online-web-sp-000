d3e80f109648ec430e7696368b3f1e0862ffaa44
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-05 04:05:07 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,3 +4,8 @@ board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 def display_board
   puts "   |   |   "
   puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+display_board(board)
