4ebbc7f0390423a3d163746d2c6c2a1b0b678d6c
Daniel Malocu <danielmalocu@yahoo.com>
2019-09-05 04:08:53 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,5 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = [" ", " ", " ", " ", "X", " ", " ", " ", " "]
 def display_board
   puts "   |   |   "
   puts "-----------"
@@ -8,4 +7,4 @@ def display_board
   puts "-----------"
   puts "   |   |   "
 end
-display_board(board)
+display_board
